39db567d4e3cd9054994fa8a343cb1e3084ba97b
zerochae <zerochae@kakao.com>
2021-10-13 11:08:15 +0900

    #Python Crud Study 1013

--- HELLO_DJANGO/HELLO_DJANGO/urls.py
+++ HELLO_DJANGO/HELLO_DJANGO/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from HELLO_DJANGO import views
+# from django.conf.urls.static import static
   
 urlpatterns = [
     # path('admin/', admin.site.urls),
